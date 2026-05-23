20481865fa707d8b605bd9e48d813d77b5f0e11d
ST4RFKR <nikelodiym1210@gmail.com>
2026-05-23 18:27:49 +0300

    8 kyu Difference of Volumes of Cuboids

---
+++ 8 kyu Difference of Volumes of Cuboids.js
@@ -0,0 +1,5 @@
+function findDifference(a, b) {
+  return Math.abs(
+    a.reduce((acc, val) => acc * val) - b.reduce((acc, val) => acc * val),
+  );
+}
--- test.js
+++ test.js
@@ -1,106 +1,14 @@
-// class Animal {
-//   constructor(name) {
-//     this.name = name;
-//   }
-//   makeSound() {
-//     console.log("Some sound");
-//   }
-// }
+function debounce(fn, delay) {
+  let timerId;
 
-// class Dog extends Animal {
-//   constructor(name) {
-//     super(name);
-//   }
-//   makeSound() {
-//     console.log(this.name + ": каже ГАВ - ГАВ");
-//   }
-// }
-
-// const bobik = new Dog("Бобік");
-// bobik.makeSound();
-// class Person {
-//   constructor(name) {
-//     this.name = name;
-//   }
-//   introduce() {
-//     console.log("my name is " + this.name);
-//   }
-// }
-
-// class Student extends Person {
-//   constructor(name) {
-//     super(name);
-//   }
-//   grade() {
-//     console.log("im study");
-//   }
-// }
-// const Bob = new Student("Bob");
-
-// Bob.introduce();
-// Bob.grade();
-// class Shape {
-//   area() {
-//     return 0;
-//   }
-// }
-// class Rectangle extends Shape {
-//   constructor(width, heigth) {
-//     // super();
-//     this.width = width;
-//     this.heigth = heigth;
-//   }
-//   area() {
-//     return this.width * this.heigth;
-//   }
-// }
-// const r = new Rectangle(5, 2);
-
-// console.log(r.area());
-
-// class Vehicle {
-//   constructor(brand) {
-//     this.brand = brand;
-//   }
-//   start() {
-//     console.log("Vehicle started");
-//   }
-// }
-
-// class Car extends Vehicle {
-//   constructor(brand, model) {
-//     super(brand);
-//     this.model = model;
-//   }
-
-//   info() {
-//     console.log(this.brand + " " + this.model);
-//   }
-// }
-// const car = new Car("BMW", "X3");
-
-// car.info();
-class Employee {
-  constructor(name, salary) {
-    this.name = name;
-    this.salary = salary;
-  }
-  work() {
-    console.log(this.name + " work in " + this.salary);
-  }
+  return function () {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => fn(), delay);
+  };
 }
-const Anna = new Employee("Anna", 2000);
-// Anna.work();
 
-class Manager extends Employee {
-  constructor(name, salary) {
-    super(name, salary);
-  }
-  manage() {
-    console.log(this.name + " work manager in " + this.salary);
-  }
-}
-const Bella = new Manager("Bella", 5000);
-// Bella.manage();
-// console.log(Bella.__proto__);
-console.log(Employee.prototype);
+const debouncedLog = debounce(() => {
+  console.log("search");
+}, 500);
+
+debouncedLog();

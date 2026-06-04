a0ba3b0f0f359375e2fd335e2056ad266a637b98
ST4RFKR <nikelodiym1210@gmail.com>
2026-06-04 11:01:02 +0300

    1047. Remove All Adjacent Duplicates In String

--- leetcode/1047. Remove All Adjacent Duplicates In String.js
+++ leetcode/1047. Remove All Adjacent Duplicates In String.js
@@ -3,22 +3,14 @@
  * @return {string}
  */
 var removeDuplicates = function (s) {
+  const stack = [];
 
-  let res = []
-
-  for (let i = 0; i < s.length; i++) {
-    const element = s[i];
-
-
-    if (res.length && res[res.length - 1] === element) {
-      res.pop()
+  for (const ch of s) {
+    if (stack.includes(ch)) {
+      stack.pop(ch);
     } else {
-      res.push(element)
+      stack.push(ch);
     }
-
   }
-  return res.join('')
+  return stack.join("");
 };
-
-console.log(removeDuplicates("abbaca"));
-

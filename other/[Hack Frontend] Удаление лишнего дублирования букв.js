412fe95d4c2a0ef57722c69433d60c1de7f8a214
ST4RFKR <nikelodiym1210@gmail.com>
2026-07-05 11:04:19 +0300

    [Hack Frontend] Удаление лишнего дублирования букв.js

---
+++ other/[Hack Frontend] Удаление лишнего дублирования букв.js
@@ -0,0 +1,15 @@
+function removeExtraLetters(str) {
+  const arr = str.split("");
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+    const next = arr[i + 1];
+
+    if (current !== next) {
+      result.push(current);
+    }
+  }
+
+  return result.join("");
+}

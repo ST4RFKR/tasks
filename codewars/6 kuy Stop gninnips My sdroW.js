a16568f5cd4e3766fdc03273e3c78e7ee70f1380
ST4RFKR <nikelodiym1210@gmail.com>
2026-07-08 09:47:37 +0300

    Stop gninnips My sdroW!

---
+++ codewars/6 kuy Stop gninnips My sdroW.js
@@ -0,0 +1,8 @@
+function spinWords(string) {
+  return string
+    .split(" ")
+    .map((word) => {
+      return word.length >= 5 ? word.split("").reverse().join("") : word;
+    })
+    .join(" ");
+}

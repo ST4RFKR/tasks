18c059c7dc1288ce75d50424ba3a2ba0a1c28ca8
ST4RFKR <nikelodiym1210@gmail.com>
2026-08-04 09:45:53 +0300

    8 kyu Multiplication table for number

---
+++ codewars/8 kyu Multiplication table for number.js
@@ -0,0 +1,11 @@
+function multiTable(number) {
+  let result = [];
+
+  for (let i = 1; i <= 10; i++) {
+    result.push(`${i} * ${number} = ${i * number}`);
+  }
+
+  return result.join("\n");
+}
+
+console.log(multiTable(5));
